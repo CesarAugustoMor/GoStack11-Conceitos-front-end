11d0b58ee56c921c7b6e263f440f7d03993e81d2
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-22 17:47:18 -0300

    Listando Projetos da api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
+
 import './App.css';
 
 import Header from './components/Header';
-import backgroundImage from './assets/background.jpg';
 
 // import { Container } from './styles';
 
 export default function App() {
-  const [projects, setProjects] = useState([
-    'Desenvolvimento de app',
-    'Front-end web',
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject(project) {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -18,10 +23,9 @@ export default function App() {
   return (
     <>
       <Header title="Projects" />
-      <img width={300} src={backgroundImage} alt="arvores somos nozes" />
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
       <button type="button" onClick={handleAddProject}>
