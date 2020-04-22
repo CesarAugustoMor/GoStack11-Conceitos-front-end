9601e209dae65217349280ae7932ab827f759f1d
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-22 16:39:52 -0300

    Propriedades

--- src/App.js
+++ src/App.js
@@ -5,5 +5,20 @@ import Header from './components/Header';
 // import { Container } from './styles';
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header title="HomePage">
+        <ul>
+          <li>HomePage</li>
+          <li>Projetos</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>HomePage</li>
+          <li>Projetos</li>
+        </ul>
+      </Header>
+    </>
+  );
 }
