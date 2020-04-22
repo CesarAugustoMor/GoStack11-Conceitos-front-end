2b158d3a4246aa69977c5d08a37a4650784f7065
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-22 17:27:50 -0300

    importando css e imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import './App.css';
 
 import Header from './components/Header';
+import backgroundImage from './assets/background.jpg';
 
 // import { Container } from './styles';
 
@@ -16,6 +18,7 @@ export default function App() {
   return (
     <>
       <Header title="Projects" />
+      <img width={300} src={backgroundImage} alt="arvores somos nozes" />
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
